57b9182c1cfaabdf7563c1cf78ababcd51373905
aliza13 <alizaajc13@gmail.com>
2022-11-02 19:05:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,21 @@
 
 import pandas as pd 
 import yfinance as yf 
-
+import datetime 
 
 
 tickerdata = yf.Ticker(tickersymbol)
 tickerinfo = tickerdata.info
+
+""" 
+def get_ticker()
+    userinput = input("What ticker symbol").uppercase
+    datetime 
+    yfCall = yf.Ticker(userinput)
+    tickerInfo = yfCall.info
+# tickername.dividends
+# use period 1m or 30m etc
+# start/end parameters "
+# 
+
+"""
