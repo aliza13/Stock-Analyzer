14dfa065b53abda4c0cb3779d52f614c821a2390
aliza13 <alizaajc13@gmail.com>
2022-11-13 23:08:17 -0700

    Updating funcs

--- main.py
+++ main.py
@@ -1,36 +1,67 @@
 # Stock Analyzer Project 
 
+from tracemalloc import start
 import pandas as pd 
 import yfinance as yf 
 import datetime 
 
+#aTick = yf.Ticker('APPL').info
+#print(aTick)
 
- # input and output
+tickers = []
 
 def getTick():
-    "This function asks the user for input and returns what ticker they are requesting"
-    userTicker = input("Type ticker of stock information to see: ")
-    while userTicker != str:
-            print("That is not a real ticker")
-            userTicker = input("Type ticker of stock information to see: ")
+    "This function asks the user for input and returns what ticker they requested"
+    howMany = int(input("How many stocks do you want to compare?: "))
+    # how to make howMany only go up to 4
+    while howMany <= howMany:
+        # needs to be stricter on input
+        userTicker = input("Type ticker of stock information to see: ")
+        # file that has all ticker names... if input != a name in the list then
+        # recalls userTicker
+        tickers.append(str(userTicker))
+        if len(tickers) > howMany -1:
             break
+        
+getTick()        
+print(tickers)
 
-    return userTicker
+def startDate():
+    start = input("Start date? YYYY-MM-DD format: ")
+    return start 
 
+def endDate():
+    end = input("End date? YYYY-MM-DD format: ")
+    return end 
 
-whateverTick = getTick()
+startAt = startDate()
+endAt = endDate()
+# need to iterate through tickers though
+# df = yf.Ticker(tickers)
 
+# def stockInfo():
+#     # shows dividends 
+#     whateverTick.dividends
+#     # quarterly earnings
+#     whateverTick.quarterly_earnings
+#     # .history(period="1mo") etc 1d, 5d, 2y, ytd, max
+#     # data = yf.download("SPY AAPL MSFT", start="2019-08-30", end="2020-01-31")
+# 
 
-#tickerdata = yf.Ticker(whateverTick)
-#tickerinfo = tickerdata.info
-#print(tickerinfo)
+# def yfCall():
+#     """Returns stock info"""
+#     tickerdata = yf.Ticker(whateverTick)
+#     tickerinfo = tickerdata.info
+#     # can do whateverTick.info["dayHigh"] or "fiftyTwoWeekLow" etc
+#     #print(tickerinfo)
+#     return tickerinfo 
+# 
+# infoOfStock = yfCall()
+# mydf = pd.DataFrame(data= infoOfStock, columns= ['Stock', 'currentPrice'])
+# print(mydf)
+
 
 """ 
-def get_ticker()
-    userinput = input("What ticker symbol").uppercase
-    datetime 
-    yfCall = yf.Ticker(userinput)
-    tickerInfo = yfCall.info
 # tickername.dividends
 # use period 1m or 30m etc
 # start/end parameters "
--- pracPandas.py
+++ pracPandas.py
@@ -46,30 +46,6 @@ JSON keys from Yahoo Finance API
 ex. ['symbol', 'longName', 'sector' 'industry','city', 'state', 'regularMarketPreviousClose']
 
 
-def getCompanyInfo(tickers, df_cols, api_cols):
-    complete_list = [] #List to hold the lists generated for each row of values that we will use in company_df
-
-    #Loops through each ticker, gets all the information, and pulls the values from the desired feilds in api_col_list
-    for symbol in tickers:
-        ticker = yf.Ticker(symbol)
-        company_info = ticker.info
-        val_list = []
-
-    #Appends values for each api_column in api_column_list for a ticker and appends them to a list for that ticker
-        for api_column in api_cols:
-            val_list.append(company_info[api_column])
-
-    #Appends the list of values for a ticker to the complete_list
-        complete_list.append(val_list)
-
-    #Generate and print dataframe with lists of ticker values
-    company_df = pd.DataFrame(complete_list, columns=df_cols)
-    print(company_df)
-    pd.company_df.to_csv()
-
-getCompanyInfo(tickers_list, df_col_list, api_col_list)
-
-
 def searchTicker(tickersymbol):
     tickerdata = yf.Ticker(tickersymbol)
     tickerinfo = tickerdata.info
